babb5a2e8ad2aa1c07f4073335357833da1a9368
Tomás Navarro <tomy.brm@gmail.com>
2023-04-28 10:58:19 -0300

    Script works for 3 devices

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import paho.mqtt.client as mqtt
 from random import randint
 from secrets import secrets
 import time
+import threading
 
-# ACCESO A DISPOSITIVO EN THINGSBOARD
-DEVICE = secrets.get('device_token')
-broker = secrets.get('broker')
-port = secrets.get('port')
+devices = [
+    {
+        "name": "maquina_1",
+        "token": secrets.get('maq_1'),
+        "broker": secrets.get('broker'),
+        "port": secrets.get('port')
+    },
+    {
+        "name": "maquina_2",
+        "token": secrets.get('maq_2'),
+        "broker": secrets.get('broker'),
+        "port": secrets.get('port')
+    },
+    {
+        "name": "maquina_3",
+        "token": secrets.get('maq_3'),
+        "broker": secrets.get('broker'),
+        "port": secrets.get('port')
+    },
+    # agregar más dispositivos según sea necesario
+]
 
 
-# Respuesta cuando el cliente recibe Connect Acknowledgement (CONNACK MSG)
 def on_connect(client, userdata, flags, rc):
     print("Connection OK" if rc == 0 else print("Connection FAIL"))
 
@@ -29,24 +46,28 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
 
-maquina_1 = mqtt.Client()
-maquina_1.on_connect = on_connect
-maquina_1.on_log = on_log
-maquina_1.on_publish = on_publish
-maquina_1.on_message = on_message
-maquina_1.username_pw_set(DEVICE)
-maquina_1.connect(broker, port, keepalive=60)
-maquina_1.loop_start()  # Sin esto no se ejecutan las funciones callback
+clients = []
+for device in devices:
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_log = on_log
+    client.on_publish = on_publish
+    client.on_message = on_message
+    client.username_pw_set(device["token"])
+    client.connect(device["broker"], device["port"], keepalive=60)
+    client.loop_start()
+    clients.append(client)
 
 # SENDING DATA TO DEVICE
 
 while True:
-    pya_val = 1  # randint(0, 1)
-    ppm_val = randint(1, 100)
-    payload = "{"
-    payload += "\"ppm\":" + str(pya_val) + ","
-    payload += "\"pya\":" + str(ppm_val)
-    payload += "}"
-    ret = maquina_1.publish("v1/devices/me/telemetry", payload)
-    print(payload)
+    for client in clients:
+        pya = randint(0, 1)
+        ppm = randint(0, 100)
+        payload = "{"
+        payload += "\"PPM2\":" + str(ppm) + ","
+        payload += "\"PYA1\":" + str(pya)
+        payload += "}"
+        ret = client.publish("v1/devices/me/telemetry", payload)
+        print(payload)
     time.sleep(5)
